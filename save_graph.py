f1b64acc08ed470e75c797e26932eeb6c2a7dd3f
jbelter <263277@student.pwr.edu.pl>
2021-12-04 23:33:21 +0100

    Add overpass endpoint

--- save_graph.py
+++ save_graph.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import osmnx as ox
 
-ox.utils.config(nominatim_endpoint='nominatim')
+ox.utils.config(nominatim_endpoint='nominatim', overpass_endpoint='overpass')
 G = ox.graph_from_place("Monte carlo", network_type="bike")
 ox.save_graphml(G)
