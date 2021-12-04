d5281464f264f6e5c82b918215bb1415498a06a8
jbelter <263277@student.pwr.edu.pl>
2021-12-05 00:28:26 +0100

    Add script for saving raw xml file to graphml and fix nominatim endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nginx:latest
+FROM gboeing/osmnx:latest
 
 COPY ./monaco-latest.osm.pbf /osmnx_data/shared/monaco-latest.osm.pbf
 
--- save_graph.py
+++ save_graph.py
@@ -1,6 +1,12 @@
-import networkx as nx
 import osmnx as ox
 
-ox.utils.config(nominatim_endpoint='nominatim', overpass_endpoint='overpass')
+ox.utils.config(
+    nominatim_endpoint='http://nominatim:8080',
+#    data_folder='/osmnx_data/shared/',
+#    overpass_endpoint='http://overpass/api',
+)
 G = ox.graph_from_place("Monte carlo", network_type="bike")
 ox.save_graphml(G)
+
+GG = ox.graph.graph_from_xml('/osmnx_data/shared/monaco-latest.osm')
+ox.save_graphml(GG, 'monaco')
