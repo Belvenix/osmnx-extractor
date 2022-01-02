3c981d334ea90fa91f3c26db44136f0eee9e202b
Vladimir Zaigrajew <vladimir.zaigrajew@cgtvx.com>
2022-01-03 00:37:59 +0100

    LAter

--- extract.sh
+++ extract.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "Usage ./extract.sh -C <continent> -c <country> -i <year interval for updating osm> -b <begin year> -e <end year>"
-while getopts C:c:i:b:e: flag
+echo "Usage ./extract.sh -d csv_file -i <year interval for updating osm> -b <begin year> -e <end year>"
+while getopts d:i:b:e: flag
 do
     case "${flag}" in
-	C) continet=${OPTARG};;
-        c) country=${OPTARG};;
+        d) csv_file=${OPTARG};;
 	i) interval=${OPTARG};;
 	b) begin=${OPTARG};;
 	e) end=${OPTARG};;
@@ -22,43 +21,49 @@ then
 	python3 ./oauth_cookie_client.py -o output_cookie.txt -s settings.json
 fi
 
-if [ ! -f ./osh_files/${continet}_${country}.osh.pbf ]
-then
-	curl -b $(cat output_cookie.txt | cut -d ';' -f 1) https://osm-internal.download.geofabrik.de/${continet}/${country}-internal.osh.pbf --output ./osh_files/${continet}_${country}.osh.pbf
-else
-	echo "Reausing file ./osh_files/${continet}_${country}.osh.pbf"
-fi
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while IFS="," read -r continet country city relation_id
+do
+	echo "Scrapping ${continet} ${country} ${city} with relation id ${relation_id}"
+	if [ ! -f ./osh_files/${continet}_${country}.osh.pbf ]
+	then
+        	curl -b $(cat output_cookie.txt | cut -d ';' -f 1) https://osm-internal.download.geofabrik.de/${continet}/${country}-internal.osh.pbf --output ./osh_files/${continet}_${country}.osh.pbf
+	else
+        	echo "Reausing file ./osh_files/${continet}_${country}.osh.pbf"
+	fi
 
-if [ -s ./osh_files/${continet}_${country}.osh.pbf ]
-then
-	echo "Converting osh to osm"
-	for ((year = $begin ; year <= $end ; year++ ))
-	do
-		echo "Creating osm for timestamp ${year}-01-01T00:00:00Z"
-		osmium time-filter -o ./osm_files/${continet}_${country}_${year}_01_01.osm.pbf ./osh_files/${continet}_${country}.osh.pbf ${year}-01-01T00:00:00Z
-		osmconvert ./osm_files/${continet}_${country}_${year}_01_01.osm.pbf -o=./osm_files/${continet}_${country}_${year}_01_01.osm
-		bzip2 -k ./osm_files/${continet}_${country}_${year}_01_01.osm
+	if [ -s ./osh_files/${continet}_${country}.osh.pbf ]
+	then
+        	echo "Converting osh to osm"
+        	for ((year = $begin ; year <= $end ; year = year + ${interval} ))
+        	do
+                	echo "Creating osm for timestamp ${year}-01-01T00:00:00Z"
+                	osmium time-filter -o ./osm_files/${continet}_${country}_${year}_01_01.osm.pbf ./osh_files/${continet}_${country}.osh.pbf ${year}-01-01T00:00:00Z
+                	osmconvert ./osm_files/${continet}_${country}_${year}_01_01.osm.pbf -o=./osm_files/${continet}_${country}_${year}_01_01.osm
+                	bzip2 -k ./osm_files/${continet}_${country}_${year}_01_01.osm
 
-		echo "Starting docker-compose"
-		export OSMNX_DOCKER_FILENAME=${continet}_${country}_${year}_01_01
-		docker-compose up -d
-		sleep 30
+                	echo "Starting docker-compose"
+                	export OSMNX_DOCKER_FILENAME=${continet}_${country}_${year}_01_01
+                	docker-compose up -d
+                	sleep 30
 
-		#echo "Running grafml extractor"
-		#python3 ./save_graph.py --city "Monte carlo" --output ./graphml_files/${continet}_${city}_${place}_${year}
-	
-		echo "Tests"
-		curl http://localhost:8080/search.php?q=Monte%20Carlo
-		curl -g 'http://localhost:12345/api/interpreter?data=[out:json];area[name="Monte Carlo"];out;'
+                	echo "Running grafml extractor"
+                	python3 ./save_graph.py --relation ${relation} --output ./graphml_files/${continet}_${country}_${city}_${year}
 
-		echo "Cleaning"
-		docker-compose down
-		rm -rf ./osm_files/* ./overpass_db/*
-	done
+                	#echo "Tests"
+                	#curl http://localhost:8080/search.php?q=Monte%20Carlo
+                	#curl -g 'http://localhost:12345/api/interpreter?data=[out:json];area[name="Monte Carlo"];out;'
 
-	rm -rf ./osh_files/${continet}_${country}.osh.pbf
-	echo "Done"
-else
-	rm -rf ./osh_files/${continet}_${country}.osh.pbf
-	echo "Could not download file maybe your cookies are wrong or your connection is bad"
-fi
+                	echo "Cleaning"
+                	docker-compose down
+                	rm -rf ./osm_files/* ./overpass_db/*
+        	done
+
+        	rm -rf ./osh_files/${continet}_${country}.osh.pbf
+        	echo "Done"
+	else
+        	rm -rf ./osh_files/${continet}_${country}.osh.pbf
+        	echo "Could not download file maybe your cookies are wrong or your connection is bad"
+	fi
+
+done < ${csv_file}
--- save_graph.py
+++ save_graph.py
@@ -2,25 +2,53 @@ import argparse
 
 import networkx as nx
 import osmnx as ox
+import overpy as op
+import shapely.geometry as geometry
+
+from shapely.ops import linemerge, unary_union, polygonize
 
 
-def get_graph(city: str, output: str) -> None:
+def get_graph(relation: int, output: str) -> None:
+    query = f"""[out:json][timeout:25];
+    rel({relation});
+    out geom;
+    >;
+    out skel qt; """
+    api = op.Overpass()
+    result = api.query(query)
+
+    rel = result.get_relations()[0]
+    lss = []
+    for ii_w, way in enumerate(rel.members[1:]):
+        ls_coords = []
+
+        for node in way.geometry:
+            ls_coords.append((node.lon, node.lat))
+
+        lss.append(geometry.LineString(ls_coords))
+
+    merged = linemerge([*lss])
+    borders = unary_union(merged)
+    polygons = list(polygonize(borders))
+
+    city = polygons[0]
+
     ox.utils.config(
         nominatim_endpoint='http://nominatim:8080',
         overpass_endpoint='http://overpass:12345',
     )
 
-    G = ox.graph_from_place(city, network_type="bike")
+    G = ox.graph_from_polygon(city, network_type="bike")
     ox.save_graphml(G, filepath=output)
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", '--city', type=str, required=True,
-            help= "Which city to get the graph")
+    parser.add_argument("-r", '--relation', type=int, required=True,
+            help= "Relation id of the city")
     parser.add_argument("-o", '--output', type=str, required=True,
             help= "Graphml output path")
     args = parser.parse_args()
 
-    get_graph(args.city, args.output)
+    get_graph(args.relation, args.output)
