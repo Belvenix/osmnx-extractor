2fd1a8805ee810c0f5b3389a319daab8c416f63c
Vladimir Zaigrajew <vladimir.zaigrajew@cgtvx.com>
2022-02-07 00:16:01 +0100

    Finished doing pipeline for semestr 2

--- clean_graph.py
+++ clean_graph.py
@@ -5,17 +5,34 @@ import osmnx as ox
 
 
 def clean_graph(first, second) -> bool:
+    print(f"---------Chaking difference of {second.name} in {first.name}")
+    
+    dif_nodes = set(second) - set(first)
+    if len(dif_nodes) == 0:
+        print("---------There are no new nodes but still checking")
+    
     dif_edges = set(second.edges()) - set(first.edges())
-    if len(dif_edges) == 0:
-        dif_nodes = set(second) - set(first)
-        if len(dif_nodes) == 0:
-            return True
-
-    return False
+    if len(dif_edges) != 0:
+        return False
+        
+    print("---------There are no new edges so checking edge data")
+    
+    for edge in set(second.edges()):
+        for x in second.edges([edge[0], edge[1]], data=True):
+            save=True
+            for y in first.edges([edge[0], edge[1]], data=True):
+                if x[2] == y[2]:
+                    save=False
+                
+            if save:
+                print("---------We found some new edge data so saving it")
+                return False
+        
+    return True
 
 
 def main():
-    files = os.listdir('graphml_files')
+    files = os.listdir('./graphml_files')
     values = set(map(lambda x: '_'.join(x.split('_')[:-1]), files))
     files_group = [[y for y in files if x in y] for x in values]
 
@@ -30,7 +47,7 @@ def main():
                 second = ox.io.load_graphml(full_path)
                 result = clean_graph(first, second)
                 if result == True:
-                    print(f"Removing {full_path}")
+                    print(f"Removing {full_path} !!!!!!!!!!!!!!!!")
                     os.remove(full_path)
                 else:
                     first = second
--- extract.sh
+++ extract.sh
@@ -22,14 +22,25 @@ then
 fi
 
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while IFS="," read -r continet country city
+while IFS="," read -r continet country sub city
 do
-	echo "Scrapping ${continet} ${country} ${city}"
-	if [ ! -f ./osh_files/${continet}_${country}.osh.pbf ]
-	then
-        	curl -b $(cat output_cookie.txt | cut -d ';' -f 1) https://osm-internal.download.geofabrik.de/${continet}/${country}-internal.osh.pbf --output ./osh_files/${continet}_${country}.osh.pbf
+	if [ -f ./output_cookie.txt ];then
+		rm ./output_cookie.txt
+	fi
+
+	echo "Scrapping ${continet} ${country} ${sub} ${city}"
+	if [ ! -f ./osh_files/${continet}_${country}.osh.pbf ];then
+		echo "Updating cookies file"
+        	python3 ./oauth_cookie_client.py -o output_cookie.txt -s settings.json
+		
+		if [ "$sub" = "_" ];then
+			curl -b $(cat output_cookie.txt | cut -d ';' -f 1) https://osm-internal.download.geofabrik.de/${continet}/${country}-internal.osh.pbf --output ./osh_files/${continet}_${country}.osh.pbf
+		else
+			echo "Using sub region https://osm-internal.download.geofabrik.de/${continet}/${country}/${sub}-internal.osh.pbf"
+			curl -b $(cat output_cookie.txt | cut -d ';' -f 1) https://osm-internal.download.geofabrik.de/${continet}/${country}/${sub}-internal.osh.pbf --output ./osh_files/${continet}_${country}.osh.pbf
+		fi
 	else
-        	echo "Reausing file ./osh_files/${continet}_${country}.osh.pbf"
+		echo "Reausing file ./osh_files/${continet}_${country}.osh.pbf"
 	fi
 
 	if [ -s ./osh_files/${continet}_${country}.osh.pbf ]
@@ -41,33 +52,46 @@ do
                 	osmium time-filter -o ./osm_files/${continet}_${country}_${year}_01_01.osm.pbf ./osh_files/${continet}_${country}.osh.pbf ${year}-01-01T00:00:00Z
                 	osmconvert ./osm_files/${continet}_${country}_${year}_01_01.osm.pbf -o=./osm_files/${continet}_${country}_${year}_01_01.osm
                 	bzip2 -k ./osm_files/${continet}_${country}_${year}_01_01.osm
-
-			chmod 666 ./osm_files/${continet}_${country}_${year}_01_01.osm
-			chmod 666 ./osm_files/${continet}_${country}_${year}_01_01.osm.pbf
-			chmod 666 ./osm_files/${continet}_${country}_${year}_01_01.osm
 			
+			attempt=0
+			wait_time=10
+			finished=0
+			while [ $attempt -le 6 && $finished -eq 0 ];do
+                		echo "Starting docker-compose for ${attempt}/6"
+              			export OSMNX_DOCKER_FILENAME=${continet}_${country}_${year}_01_01
+                		docker-compose up -d
+				current_time=0
+				while [ $current_time -le $wait_time && $finished -eq 0 ];do
+					if [ $(curl http://localhost:8080/search.php?q=${city} | wc -c) -gt 5 && $(curl -g 'http://localhost:12345/api/interpreter?data=[out:json];area[name="${city}"];out;') -gt 350 ];then 
+						finished=1
+					fi
+                			sleep 5m
+					current_time=$(( $current_time + 5 ))
+				done
+				wait_time=$(($wait_time * 2 ))
+				attempt=$(($attempt + 1))
+				docker-compose down
+				rm -rf ./overpass_db/*
+			done
 
-                	echo "Starting docker-compose"
-                	export OSMNX_DOCKER_FILENAME=${continet}_${country}_${year}_01_01
-                	docker-compose up -d
-                	sleep 60
-
-                	echo "Running grafml extractor"
-                	python3 ./save_graph.py --city ${city} --output ${continet}_${country}_${city}_${year}
-
-                	#echo "Tests"
-                	#curl http://localhost:8080/search.php?q=Monte%20Carlo
-                	#curl -g 'http://localhost:12345/api/interpreter?data=[out:json];area[name="Monte Carlo"];out;'
+			if [ $finished -eq 1 ];then
+           			echo "Running grafml extractor"
+                		python3 ./save_graph.py --city ${city} --output ${continet}_${country}_${city}_${year}
+			else
+				echo "Could not download ${continet}_${country}_${city}_${year} graph"
+			fi
 
                 	echo "Cleaning"
-                	docker-compose down
-                	rm -rf ./overpass_db/* #./osm_files/*
+                	rm -rf ./cache #./osm_files/*
         	done
 
         	echo "Done"
 	else
         	echo "Could not download file maybe your cookies are wrong or your connection is bad"
 	fi
+	rm -rf ./osh_files/*
+	exit
 
 done < ${csv_file}
-rm -rf ./osh_files/*
+echo "Cleaining graph process started"
+python3 clean_graph.py
